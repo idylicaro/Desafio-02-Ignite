8e437d00b16879d807fa54d8fe5a58f5832a5a4c
Idyl Icaro dos Santos <idylicaro.se@gmail.com>
2023-07-09 19:46:23 -0300

    feat: init users routes

--- src/@Types/knex.d.ts
+++ src/@Types/knex.d.ts
@@ -1,8 +1,25 @@
 // eslint-disable-next-line
+import { UUID } from 'crypto';
 import { Knex } from 'knex';
 
 declare module 'knex/types/tables' {
   export interface Tables {
     // tables here {key: type}
+    users: {
+      id: UUID;
+      username: string;
+      email: string;
+      count_meals: number;
+      count_meals_in_diet: number;
+      session_id?: string;
+    };
+    meals: {
+      id: UUID;
+      user_id: UUID;
+      name: string;
+      description: string;
+      date_time: Date;
+      in_diet: boolean;
+    };
   }
 }
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
 import fastify from 'fastify';
 
-// import { transactionsRoutes } from './routes/transactions';
+import { usersRoutes } from './routes/users';
 
 export const app = fastify();
 
-// app.register(transactionsRoutes, {
-//   prefix: 'transactions'
-// });
+app.register(usersRoutes, {
+  prefix: 'users'
+});
